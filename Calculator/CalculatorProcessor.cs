bc853754a9cd9d206b663939a3a52e538d1cbeaf
ramees-kr <ramees.kr@outlook.com>
2024-04-14 02:34:04 -0400

    Added Files

---
+++ Calculator/CalculatorProcessor.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    internal class CalculatorProcessor
+    {
+        public static double Process(double operator1, double operator2, string operand)
+        {
+            switch (operand)
+            {
+                case "+":
+                    return operator1 + operator2;
+                case "-":
+                    return operator1 - operator2;
+                case "x":
+                    return operator1 * operator2;
+                case "/":
+                    if (operator2 != 0)
+                        return operator1 / operator2;
+                    else
+                        throw new DivideByZeroException("Cannot divide by zero");
+                default:
+                    throw new ArgumentException("Invalid operand");
+            }
+        }
+
+    }
+}
---
+++ Calculator/MainPage.xaml.cs
@@ -0,0 +1,85 @@
+﻿namespace Calculator
+{
+    public partial class MainPage : ContentPage
+    {
+        private string currentInput = "0";
+        private string currentOperator;
+        private double firstOperand;
+        private double secondOperand;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            resultLabel.Text = currentInput;
+        }
+
+        private void NumberButton_Clicked(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            string buttonText = button.Text;
+
+            if (currentInput == "0" || currentInput == "Error")
+            {
+                currentInput = buttonText;
+            }
+            else
+            {
+                currentInput += buttonText;
+            }
+
+            resultLabel.Text = currentInput;
+        }
+
+        private void OperatorButton_Clicked(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            currentOperator = button.Text;
+            firstOperand = double.Parse(currentInput);
+            currentInput = "0";
+            resultLabel.Text = currentInput;
+        }
+
+        private void ClearButton_Clicked(object sender, EventArgs e)
+        {
+            currentInput = "0";
+            currentOperator = null;
+            firstOperand = 0;
+            secondOperand = 0;
+            resultLabel.Text = currentInput;
+        }
+
+        private void BackspaceButton_Clicked(object sender, EventArgs e)
+        {
+            if (currentInput.Length > 1)
+            {
+                currentInput = currentInput.Remove(currentInput.Length - 1);
+            }
+            else
+            {
+                currentInput = "0";
+            }
+            resultLabel.Text = currentInput;
+        }
+
+        private void EqualsButton_Clicked(object sender, EventArgs e)
+        {
+            if (currentOperator != null)
+            {
+                secondOperand = double.Parse(currentInput);
+                try
+                {
+                    double resultLabelValue = CalculatorProcessor.Process(firstOperand, secondOperand, currentOperator);
+                    currentInput = resultLabelValue.ToString();
+                    resultLabel.Text = currentInput;
+                }
+                catch (Exception ex)
+                {
+                    currentInput = "Error";
+                    resultLabel.Text = currentInput;
+                    Console.WriteLine($"Error occurred: {ex.Message}");
+                }
+            }
+        }
+    }
+
+}
